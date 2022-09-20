6983ad4b758e957e6f113ab2c629df6327579efc
Bryan Lucas Skalski <bryan.lucas.skalski@gmail.com>
2022-09-20 10:24:27 +0200

    Fonctions f& et f2 mises à la fin du code     * prototypes mis au début

--- MTR/main.c
+++ MTR/main.c
@@ -4,6 +4,8 @@
 //  
 //
 /*--------------------------------------------*/
+
+
 //	Création de Thread
 //#include <pthread.h>
 //int pthread_create(pthread_t* restrict thread,
@@ -12,8 +14,28 @@
 //	void* restrict arg);
 // 
 // deux thread, sync on end ---- /!\ lier avec -lpthread
+
+
 #include <pthread.h>
 #include <stdio.h>
+
+int* f1(int* pi);
+int* f2(int* pi);
+
+int main() {
+	int a, b;
+	int* ret1 = 0, * ret2 = 0;
+	pthread_t th1, th2;
+	a = 10;
+	b = 20;
+	/* création des deux threads */
+	/* Attendre la fin de leur exécutions */
+	printf("main) th1 : *ret1 = %3d, &res = %p\n", *ret1, ret1);
+	printf("main) th2 : *ret2 = %3d, &res = %p\n", *ret2, ret2);
+	return 0;
+}
+
+
 int* f1(int* pi) {
 	static int res = 0;
 	res = *pi + 1;
@@ -27,16 +49,4 @@ int* f2(int* pi) {
 	printf("th2 : res = %3d, pi = %p, &res = %p, *pi = %3d\n",
 		res, pi, &res, *pi);
 	return &res;
-}
-int main() {
-	int a, b;
-	int* ret1 = 0, * ret2 = 0;
-	pthread_t th1, th2;
-	a = 10;
-	b = 20;
-	/* création des deux threads */
-	/* Attendre la fin de leur exécutions */
-	printf("main) th1 : *ret1 = %3d, &res = %p\n", *ret1, ret1);
-	printf("main) th2 : *ret2 = %3d, &res = %p\n", *ret2, ret2);
-	return 0;
 }
