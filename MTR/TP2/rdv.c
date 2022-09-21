dc5dea3f040c92fcc0df350e7ef289fc8241b079
Bryan Lucas Skalski <bryan.lucas.skalski@gmail.com>
2022-09-21 12:21:34 +0200

    Code thread3 | Rendez-vous finis

--- MTR/TP2/rdv.c
+++ MTR/TP2/rdv.c
@@ -3,14 +3,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <semaphore.h>
+
+#define N 3
+
 pthread_t tid[3];
-...
-...
+
+//Initialisation sémaphores et variable
+sem_t MUTEX, ATTENTE;
+int nb_att = 0;
+
+void b_sync();
+
 void f1() {
 	sleep(1); printf("(1)f1 synchronizing ...\n");
 	sleep(1); printf("(2)f1 synchronizing ...\n");
 	sleep(1); printf("(3)f1 synchronizing ...\n");
-	/* Attente du RDV */
+	b_sync();
 	printf("f1 synchronization done ...\n");
 	/* instructions ... */
 }
@@ -20,21 +29,52 @@ void f2() {
 	sleep(1); printf("(3)f2 synchronizing ...\n");
 	sleep(1); printf("(5)f2 synchronizing ...\n");
 	sleep(1); printf("(6)f2 synchronizing ...\n");
-	/* Attente du RDV */
+	b_sync();
 	printf("f2 synchronization done ...\n");
 	/* instructions ... */
 }
 void f3() {
 	sleep(1); printf("(1)f3 synchronizing ...\n");
-	/* Attente du RDV */
+	b_sync();
 	printf("f3 synchronization done ...\n");
 	/* instructions ... */
 }
+
+void b_sync() {
+	/*------------------------------*/
+	//Sémaphore MUTEX rendu
+	sem_wait(&MUTEX);
+	//Incrémentation de nb_att --> Une tâche a pris le MUTEX
+	nb_att++;
+	/*------------------------------*/
+	if (nb_att == N) { // SI N tâches ont prisent le MUTEX
+		/*------------------------------*/
+		//Prise du sémaphore ATTENTE N fois
+		for (int i = 1; i <= N; i++) {
+			sem_post(&ATTENTE);
+		}
+		/*------------------------------*/
+		nb_att = 0;
+		//Prise du sémaphore MUTEX de nouveau
+		sem_post(&MUTEX);
+	}
+	else { //Sinon
+		/*------------------------------*/
+		//Reprise du MUTEX et ATTENTE rendu
+		sem_post(&MUTEX);
+		sem_wait(&ATTENTE);
+		/*------------------------------*/
+	}
+}
+
 int main() {
-	int i = 0;
 	int err;
-	...
-		err = pthread_create(&(tid[0]), NULL, (void* (*)(void*))f1, NULL);
+	/*------------------------------*/
+	//Initialisation des sémaphores
+	sem_init(&ATTENTE, 0, 0);
+	sem_init(&MUTEX, 0, 1);
+	/*------------------------------*/
+	err = pthread_create(&(tid[0]), NULL, (void* (*)(void*))f1, NULL);
 	if (err != 0)
 		printf("\ncan't create thread :[%s]", strerror(err));
 	err = pthread_create(&(tid[1]), NULL, (void* (*)(void*))f2, NULL);
@@ -48,6 +88,10 @@ int main() {
 	pthread_join(tid[0], NULL);
 	pthread_join(tid[1], NULL);
 	pthread_join(tid[2], NULL);
-	...
+	/*------------------------------*/
+	//Déstruction des sémaphores
+	sem_destroy(&ATTENTE);
+	sem_destroy(&MUTEX);
+	/*------------------------------*/
 		return 0;
 }
