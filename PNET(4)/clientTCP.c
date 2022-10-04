4d0d4c80eadcc7f4b998d12aec629f465a97247c
Bryan Lucas Skalski <bryan.lucas.skalski@gmail.com>
2022-10-04 15:17:14 +0200

    edit FONCTIONEL

--- PNET(4)/clientTCP.c
+++ PNET(4)/clientTCP.c
@@ -13,8 +13,6 @@ int main(int argc, char** argv, char** env) {
 		return 1;
 	}
 
-	int buffer[sizeof(argv[1])];
-
 	struct sockaddr_in to;
 	struct hostent* toinfo;
 	int desc = socket(AF_INET, SOCK_STREAM, 0);
@@ -29,7 +27,8 @@ int main(int argc, char** argv, char** env) {
 		return -1;
 	}
 	memcpy(&to.sin_addr.s_addr, toinfo->h_addr_list[0], toinfo->h_length);
-	printf("%d.%d.%d.%d", to.sin_addr.s_addr & 0xFF000000, to.sin_addr.s_addr & 0x00FF0000 >> 8, to.sin_addr.s_addr & 0x0000FF00 >> 16, to.sin_addr.s_addr & 0x000000FF >> 24);
+	//printf("%d.%d.%d.%d", to.sin_addr.s_addr & 0xFF000000, to.sin_addr.s_addr & 0x00FF0000 >> 8, to.sin_addr.s_addr & 0x0000FF00 >> 16, to.sin_addr.s_addr & 0x000000FF >> 24);
+	//printf("%ls\n", toinfo->h_addr_list[0] & 0x0000FF000);
 	to.sin_port = htons(7777);
 
 	if (connect(desc, (struct sockaddr*)&to, sizeof(to)) == -1) {
@@ -37,19 +36,20 @@ int main(int argc, char** argv, char** env) {
 		return -1;
 	}
 	
-	int rtr = send(desc, argv[1], sizeof(argv[1]), 0);
+	int rtr = send(desc, argv[2], sizeof(argv[2]), 0);
 	if (rtr == -1) {
 		perror("erreur d'envoi");
 		return -1;
 	}
 
+	int buffer[sizeof(argv[1])];
 	rtr = recv(desc, buffer, sizeof(buffer), 0);
 	if (rtr == -1) {
 		perror("erreur de reception");
 		return -1;
 	}
 
-	printf("Sortie : %ls", buffer);
+	printf("Sortie : %s\n", buffer);
 
 	//rtr = close(desc);
 	//if (rtr == -1) {
