37a2a13bfda09bdbbdd3cabeb266ae8e69931f3c
Bryan Lucas Skalski <bryan.lucas.skalski@gmail.com>
2022-10-07 09:07:36 +0200

    TD-UDP

---
+++ PNET(4)/clientUdpDaytime.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+/* ... */
+int main(int argc, char** argv, char** env) {
+	struct sockaddr_in to;
+	struct hostent* toinfo;
+	//TODO
+	if (argc < 2) {
+		fprintf(stderr, "Format : %s <serveur>\n", argv[0]);
+		return(1);
+	}
+	int desc = socket(AF_INET, SOCK_DGRAM, 0);
+	if (desc == -1) {
+		perror("Erreur socket");
+		return -1;
+	}
+	to.sin_family = AF_INET;
+	toinfo = gethostbyname(argv[1]);
+	if (toinfo == NULL) {
+		perror("Erreur de gethostbyname()");
+		return -1;
+	}
+	memcpy(&to.sin_addr.s_addr, toinfo->h_addr_list[0], toinfo->h_length);
+	to.sin_port = htons(7777);
+	
+	return (0);
+}
