4868129cb279eeb55ba7a9246c7abf682ed1c1fc
Bryan Lucas Skalski <bryan.lucas.skalski@gmail.com>
2022-10-07 11:21:34 +0200

    client UDP Fonctionnel

--- PNET(4)/clientUdpDaytime.c
+++ PNET(4)/clientUdpDaytime.c
@@ -2,31 +2,60 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
 
-#define PORT 7777
-
-/* ... */
 int main(int argc, char** argv, char** env) {
 	struct sockaddr_in to;
 	struct hostent* toinfo;
-	//TODO
+	/* to */
 	if (argc < 2) {
 		fprintf(stderr, "Format : %s <serveur>\n", argv[0]);
 		return(1);
 	}
-	int desc = socket(AF_INET, SOCK_DGRAM, 0);
-	if (desc == -1) {
-		perror("Erreur socket");
-		return -1;
-	}
+
 	to.sin_family = PF_INET;
 	toinfo = gethostbyname(argv[1]);
 	if (toinfo == NULL) {
 		perror("Erreur de gethostbyname()");
 		return -1;
 	}
+	to.sin_port = htons(13);
 	memcpy(&to.sin_addr.s_addr, toinfo->h_addr_list[0], toinfo->h_length);
-	to.sin_port = htons(PORT);
-	
+	printf("Adresse : %d.%d.%d.%d\n",
+		to.sin_addr.s_addr & 0xFF,
+		(to.sin_addr.s_addr & 0xFF00) >> 8,
+		(to.sin_addr.s_addr & 0xFF0000) >> 16,
+		(to.sin_addr.s_addr & 0xFF000000) >> 24
+	);
+	/* fin to*/
+	int desc = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	if (desc == -1) {
+		perror("Erreur socket");
+		return -1;
+	}
+	char buffer[48];
+	int rtr = sendto(desc, buffer, sizeof(buffer), 0, (struct sockaddr *)&to, sizeof(to));
+	if (rtr == -1) {
+		perror("Erreur d'envoi");
+		return -1;
+	}
+	unsigned from, fromlen;
+
+	rtr = recvfrom(desc, buffer, sizeof(buffer), 0, (struct sockaddr *)&from, &fromlen);
+	if (rtr == -1) {
+		perror("Erreur de reception");
+		return -1;
+	}
+	printf("%s\n", buffer);
+
+	rtr = close(desc);
+	if (rtr == -1) {
+		perror("Erreur de fermeture");
+		return -1;
+	}
+	/*	fin	*/
 	return (0);
 }
